491d192f7e2f0e0b642a6664794791015e0ed3a6
MiskLG <misklg283@gmail.com>
2022-10-29 15:04:16 +0200

    Added base project for 'izazov 1'

---
+++ izazovi/sc_2022_challenge_1/process.py
@@ -0,0 +1,16 @@
+# ovde importovati biblioteke
+
+
+def count_blood_cells(image_path):
+    """
+    Procedura prima putanju do fotografije i vraca broj crvenih krvnih zrnaca.
+
+    Ova procedura se poziva automatski iz main procedure i taj deo kod nije potrebno menjati niti implementirati.
+
+    :param image_path: <String> Putanja do ulazne fotografije.
+    :return: <int>  Broj prebrojanih crvenih krvnih zrnaca
+    """
+    blood_cell_count = 0
+    # TODO - Prebrojati crvena krvna zrnca i vratiti njihov broj kao povratnu vrednost ove procedure
+
+    return blood_cell_count
